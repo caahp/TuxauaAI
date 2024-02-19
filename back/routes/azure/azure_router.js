b342a981160cf6a98f4e2b31aec5050dd4f556ac
Caroline Souza <carolline.ps@hotmail.com>
2024-02-19 16:37:04 -0400

    azure tester OK

---
+++ back/routes/azure/azure_router.js
@@ -0,0 +1,36 @@
+const request = require('request');
+
+let key = '00ac08b016b04a3b9a2f1c53b04613ec';
+let endpoint = 'https://tuxaua.cognitiveservices.azure.com/';
+
+var uriBase = endpoint + 'vision/v3.1/analyze';
+
+const imageUrl =
+    'https://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg';
+
+// Request parameters.
+const params = {
+    'visualFeatures': 'Categories,Description,Color',
+    'details': '',
+    'language': 'en'
+};
+
+const options = {
+    uri: uriBase,
+    qs: params,
+    body: '{"url": ' + '"' + imageUrl + '"}',
+    headers: {
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key' : key
+    }
+};
+
+request.post(options, (error, response, body) => {
+  if (error) {
+    console.log('Error: ', error);
+    return;
+  }
+  let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
+  console.log('JSON Response\n');
+  console.log(jsonResponse);
+});
