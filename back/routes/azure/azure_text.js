d322d59d8a2aa6810f830196b93e7124f4f33964
Caroline Souza <carolline.ps@hotmail.com>
2024-02-19 16:49:08 -0400

    azure text recog

---
+++ back/routes/azure/azure_text.js
@@ -0,0 +1,35 @@
+const request = require('request');
+
+let key = '00ac08b016b04a3b9a2f1c53b04613ec';
+let endpoint = 'https://tuxaua.cognitiveservices.azure.com/';
+
+var uriBase = endpoint + 'vision/v3.1/ocr';
+
+const imageUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/' +
+    'Atomist_quote_from_Democritus.png/338px-Atomist_quote_from_Democritus.png';
+
+// Request parameters.
+const params = {
+    'language': 'unk',
+    'detectOrientation': 'true',
+};
+
+const options = {
+    uri: uriBase,
+    qs: params,
+    body: '{"url": ' + '"' + imageUrl + '"}',
+    headers: {
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key' : key
+    }
+};
+
+request.post(options, (error, response, body) => {
+  if (error) {
+    console.log('Error: ', error);
+    return;
+  }
+  let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
+  console.log('JSON Response\n');
+  console.log(jsonResponse);
+});
