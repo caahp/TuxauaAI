56cbb5b183bc106e443760c26be154181fa3fcd3
Stephanie Barroso Moreira Guimaraes <stephaniebarrosomoreira@gmail.com>
2024-02-21 18:36:32 -0400

    rotas back/front

--- tuxaua-angular-16/src/app/app.module.ts
+++ tuxaua-angular-16/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -39,7 +40,9 @@ import { FooterComponent } from './footer/footer.component';
     MatButtonModule,
     MatGridListModule,
     MatSnackBarModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
