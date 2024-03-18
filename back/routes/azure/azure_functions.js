8d7ca639dc253a032bf4cb66f6edc0875ac05a48
Caroline Pereira <carolline.ps@hotmail.com>
2024-03-18 14:42:44 -0400

    updates

--- back/routes/azure/azure_functions.js
+++ back/routes/azure/azure_functions.js
@@ -106,53 +106,52 @@ class AzureFunctions {
         });
     }
 
-    async detectDescription(body){
-        const imageUrl = body.imageUrl;
-
-        if (!imageUrl) {
-            throw new Error('Image URL is required in the request body.');
+    async detectDescription(imageBase64) {
+        if (!imageBase64) {
+            throw new Error('Image base64 data is required in the request body.');
         }
-
+    
         const uriBase = this.endpoint + 'vision/v3.1/analyze';
-
+    
         // Request parameters.
         const params = {
             'visualFeatures': 'Description',
             'language': 'en'
         };
-
+    
         const options = {
             uri: uriBase,
             qs: params,
-            body: `{"url": "${imageUrl}"}`,
+            body: JSON.stringify({ 'url': '' }),
             headers: {
-                'Content-Type': 'application/json',
+                'Content-Type': 'application/octet-stream',
                 'Ocp-Apim-Subscription-Key': this.key
-            }
+            },
+            encoding: null, // Important to keep the body as binary
+            body: Buffer.from(imageBase64, 'base64') // Convert base64 to buffer
         };
-
+    
         return new Promise((resolve, reject) => {
             request.post(options, (error, response, responseBody) => {
                 if (error) {
                     console.log('Error: ', error);
                     reject('Internal server error.');
                 }
+    
                 const list = [];
                 const jsonResponse = JSON.parse(responseBody);
-                if(jsonResponse.error) {
-                    list.push('Não foi posível obter uma descrição')
+                if (jsonResponse.error) {
+                    list.push('Não foi possível obter uma descrição');
                     resolve(list);
-                }
-                else{
+                } else {
                     list.push(jsonResponse.description.captions[0].text);
-                    list.push('confidence: ' + jsonResponse.description.captions[0].confidence);
+                    list.push('Confidence: ' + jsonResponse.description.captions[0].confidence);
                     resolve(list);
                 }
-                resolve(list);
             });
         });
     }
-
+    
     async detectObjects(body){
         const imageUrl = body.imageUrl;
 
